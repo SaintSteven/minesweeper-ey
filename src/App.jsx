e2034ce28c305acad482d0cb40e3c20c208cc56a
SaintSteven <estebandiazaranda@gmail.com>
2023-03-01 02:42:25 -0300

    [ADD] Flag icon

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Minefield from './components/Minefield/Minefield'
+
+function App() {
+
+  return (
+    <Minefield />
+  )
+}
+
+export default App
---
+++ src/components/Minefield/Minefield.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Minefield() {
+  return (
+    <div>Minefield</div>
+  )
+}
